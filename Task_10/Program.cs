6e77ef7e8a5f6c371846ae155c65abb238927b92
rillout <fedkv@gmail.com>
2022-03-06 12:15:38 +0300

    Add Task_13

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе выводит перевёрнутое число.
+
+using System;
+class Program  {
+
+   static void Main()
+
+    {
+        
+        void Revers(int currentNumber)
+        {
+            int a = currentNumber / 100;
+            int b = currentNumber % 100 / 10;
+            int c = currentNumber % 100 % 10;
+            Console.WriteLine(c * 100 + b * 10 + a);
+        }
+    
+        Console.Write("Введите 3-х значное число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        Revers(number);
+    
+    
+    }
+    
+
+}
+
