07c724588fe9064de468b5d5f6cd6bd04b548457
rillout <fedkv@gmail.com>
2022-03-06 13:40:36 +0300

    Added Task_13

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+
+    {
+
+        void FindThirdDigit(string currentNumber)
+        {
+            if (currentNumber.Length > 2)
+            {
+                Console.WriteLine(currentNumber[2]);
+            }
+            else Console.WriteLine("Третьей цифры нет");
+
+        }
+        Console.Write("Введите число: ");
+        string number = Console.ReadLine();
+        FindThirdDigit(number);
+
+    }
+
+
+}
+
